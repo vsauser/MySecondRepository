9710634600022be8606d498654d346f101012246
vsa_user <kritik.sova@gmail.com>
2023-03-19 22:11:26 +0300

    Копирование проекта в новый репозиторий

---
+++ Calculator/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Program
+{  
+    public static void Main(string[] args) 
+    {
+        string repeat = "Y";
+        do
+        {
+            Console.WriteLine("Insert number X?????????:");
+            string FirstString = Console.ReadLine();
+            int X = Convert.ToInt32(FirstString);
+            Console.WriteLine("Insert number Y?????????:");
+            string SecondString = Console.ReadLine();
+            int Y = Convert.ToInt32(SecondString);
+            Console.WriteLine("Choose operating (+, -, *, /)");
+            string Operating = Console.ReadLine();
+            if (Operating == "+")
+            {
+                int Summ = X + Y;
+                Console.WriteLine("Summ = " + Summ);
+            }
+            else if (Operating == "-")
+            {
+                int Substruct = X - Y;
+                Console.WriteLine("Substruct = " + Substruct);
+            }
+            else if (Operating == "*")
+            {
+                        int Add = X * Y;
+                        Console.WriteLine("Add = " + Add);
+            }
+            else if (Operating == "/")
+            {
+                double Div = (double)X / (double)Y;
+                Console.WriteLine("{0:F4}", Div);
+            }
+
+            Console.WriteLine("Do you want to repeat evaluation?");
+
+        }
+        while (Console.ReadLine() == "Y");
+    }  
+}
